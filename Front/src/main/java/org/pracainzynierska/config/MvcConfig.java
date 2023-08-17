9016cb54d001c4edd723524c2465b3308c364819
AnnaMalgorzata <132166200+AnnaMalgorzata@users.noreply.github.com>
2023-08-17 16:23:42 +0200

    Update MvcConfig.java

--- Front/src/main/java/org/pracainzynierska/config/MvcConfig.java
+++ Front/src/main/java/org/pracainzynierska/config/MvcConfig.java
@@ -6,16 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
-
-    /*
-        Najpierw konfigurujemy zewnętrzną ścieżkę URI, definiując handler zasobów.
-        Następnie mapujemy wewnętrznie tę zewnętrzną ścieżkę URI na fizyczną ścieżkę,
-        na której faktycznie znajdują się zasoby.
-        Można oczywiście zdefiniować wiele handlerów zasobów używając tego prostego,
-        ale elastycznego API.
-        Teraz następująca linia na stronie html da nam zasób myCss.css wewnątrz katalogu webapp/resources:
-            <link rel="stylesheet" th:href="@{css/main.css}"/>
-     */
+    
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         if (!registry.hasMappingForPattern("/static/**")) {
